3f8e216bdffdafb467424612c65f398b5c00d932
Antonio-Zago <antoniozagodev@gmail.com>
2022-05-02 08:25:37 -0300

    Initial commit

---
+++ src/model/Banco.java
@@ -0,0 +1,20 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Banco {
+	
+	private static List<Carro> carros = new ArrayList<Carro>();
+	private static List<Cliente> clientes = new ArrayList<>();
+	private static List<Vendedor> vendedores = new ArrayList<>();
+	
+	
+	
+	
+	
+	static {
+
+		
+	}
+}
---
+++ src/view/Painel.java
@@ -0,0 +1,7 @@
+package view;
+
+public class Painel {
+	public static void main(String[] args) {
+		
+	}
+}
